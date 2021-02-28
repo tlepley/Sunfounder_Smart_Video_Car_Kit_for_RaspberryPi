26469382d11f4f2d94cba9da9971960e34212ba5
tlepley <tlepley@localhost.localdomain>
2021-02-28 14:44:49 +0100

    Further cleanup and change 'home' command to 'center'

--- client/client_App.py
+++ client/client_App.py
@@ -2,13 +2,23 @@
 # -*- coding: utf-8 -*-
 import tkinter as tk
 import socket as sock      # Import necessary modules
+import sys
+from optparse import OptionParser
 
 ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
 
-top = tk.Tk()   # Create a top window
-top.title('Sunfounder Raspberry Pi Smart Video Car')
+# Handle options
+parser = OptionParser()
+parser.add_option("-s", "--server", dest="serverIP", help="server IP")
+(options, args) = parser.parse_args()
+if not options.serverIP:
+	print('The server IP must be provided')
+	sys.exit(1)
 
-HOST = '192.168.0.25'    # Server(Raspberry Pi) IP address
+topWindow = tk.Tk()   # Create a top window
+topWindow.title('Sunfounder Raspberry Pi Smart Video Car')
+
+HOST = options.serverIP    # Server(Raspberry Pi) IP address
 PORT = 21567
 BUFSIZ = 1024             # buffer size
 ADDR = (HOST, PORT)
@@ -19,120 +29,114 @@ tcpCliSock.connect(ADDR)                    # Connect with the server
 # =============================================================================
 # The function is to send the command forward to the server, so as to make the 
 # car move forward.
-# ============================================================================= 
+# =============================================================================
+def sendCmd(cmd):
+	print('send command:', cmd)
+	tcpCliSock.send(cmd.encode())
+
+
 def forward_fun(event):
-	print('forward')
-	tcpCliSock.send('forward'.encode())
+	sendCmd('forward')
 
 def backward_fun(event):
-	print('backward')
-	tcpCliSock.send('backward'.encode())
+	sendCmd('backward')
 
 def left_fun(event):
-	print('left')
-	tcpCliSock.send('left'.encode())
+	sendCmd('left')
 
 def right_fun(event):
-	print('right')
-	tcpCliSock.send('right'.encode())
+	sendCmd('right')
 
 def stop_fun(event):
-	print('stop')
-	tcpCliSock.send('stop'.encode())
+	sendCmd('stop')
 
 def home_fun(event):
-	print('home')
-	tcpCliSock.send('home'.encode())
+	sendCmd('center')
 
 def x_increase(event):
-	print('x+')
-	tcpCliSock.send('x+'.encode())
+	sendCmd('x+')
 
 def x_decrease(event):
-	print('x-')
-	tcpCliSock.send('x-'.encode())
+	sendCmd('x-')
 
 def y_increase(event):
-	print('y+')
-	tcpCliSock.send('y+'.encode())
+	sendCmd('y+')
 
 def y_decrease(event):
-	print('y-')
-	tcpCliSock.send('y-'.encode())
+	sendCmd('y-')
 
 def xy_home(event):
-	print('xy_home')
-	tcpCliSock.send('xy_home'.encode())
+	sendCmd('xy_home')
 
 # =============================================================================
 # Exit the GUI program and close the network connection between the client 
 # and server.
 # =============================================================================
 def quit_fun(event):
-	top.quit()
+	topWindow.quit()
 	tcpCliSock.send('stop'.encode())
 	tcpCliSock.close()
 
 # =============================================================================
 # Create buttons
 # =============================================================================
-Btn0 = tk.Button(top, width=5, text='Forward')
-Btn1 = tk.Button(top, width=5, text='Backward')
-Btn2 = tk.Button(top, width=5, text='Left')
-Btn3 = tk.Button(top, width=5, text='Right')
-Btn4 = tk.Button(top, width=5, text='Quit')
-Btn5 = tk.Button(top, width=5, height=2, text='Home')
+BtnFwd = tk.Button(topWindow, width=5, text='Forward')
+BtnBwd = tk.Button(topWindow, width=5, text='Backward')
+BtnLeft = tk.Button(topWindow, width=5, text='Left')
+BtnRight = tk.Button(topWindow, width=5, text='Right')
+BtnQuit = tk.Button(topWindow, width=5, text='Quit')
+BtnCenter = tk.Button(topWindow, width=5, height=2, text='Center')
 
 # =============================================================================
 # Buttons layout
 # =============================================================================
-Btn0.grid(row=0,column=1)
-Btn1.grid(row=2,column=1)
-Btn2.grid(row=1,column=0)
-Btn3.grid(row=1,column=2)
-Btn4.grid(row=3,column=2)
-Btn5.grid(row=1,column=1)
+BtnFwd.grid(row=0, column=1)
+BtnBwd.grid(row=2, column=1)
+BtnLeft.grid(row=1, column=0)
+BtnRight.grid(row=1, column=2)
+BtnQuit.grid(row=3, column=2)
+BtnCenter.grid(row=1, column=1)
 
 # =============================================================================
-# Bind the buttons with the corresponding callback function.
+# Bind the buttons for the car movements
 # =============================================================================
-Btn0.bind('<ButtonPress-1>', forward_fun)  # When button0 is pressed down, call the function forward_fun().
-Btn1.bind('<ButtonPress-1>', backward_fun)
-Btn2.bind('<ButtonPress-1>', left_fun)
-Btn3.bind('<ButtonPress-1>', right_fun)
-Btn0.bind('<ButtonRelease-1>', stop_fun)   # When button0 is released, call the function stop_fun().
-Btn1.bind('<ButtonRelease-1>', stop_fun)
-Btn2.bind('<ButtonRelease-1>', stop_fun)
-Btn3.bind('<ButtonRelease-1>', stop_fun)
-Btn4.bind('<ButtonRelease-1>', quit_fun)
-Btn5.bind('<ButtonRelease-1>', home_fun)
+BtnFwd.bind('<ButtonPress-1>', forward_fun)  # When button0 is pressed down, call the function forward_fun().
+BtnFwd.bind('<ButtonRelease-1>', stop_fun)   # When button0 is released, call the function stop_fun().
+BtnBwd.bind('<ButtonPress-1>', backward_fun)
+BtnBwd.bind('<ButtonRelease-1>', stop_fun)
+BtnLeft.bind('<ButtonPress-1>', left_fun)
+BtnLeft.bind('<ButtonRelease-1>', stop_fun)
+BtnRight.bind('<ButtonPress-1>', right_fun)
+BtnRight.bind('<ButtonRelease-1>', stop_fun)
+BtnQuit.bind('<ButtonRelease-1>', quit_fun)
+BtnCenter.bind('<ButtonRelease-1>', home_fun)
 
 # =============================================================================
-# Create buttons
+# Create buttons for the camera movements
 # =============================================================================
-Btn07 = tk.Button(top, width=5, text='X+', bg='red')
-Btn08 = tk.Button(top, width=5, text='X-', bg='red')
-Btn09 = tk.Button(top, width=5, text='Y-', bg='red')
-Btn10 = tk.Button(top, width=5, text='Y+', bg='red')
-Btn11 = tk.Button(top, width=5, height=2, text='HOME', bg='red')
+BtnXPlus = tk.Button(topWindow, width=5, text='X+', bg='red')
+BtnXMinus = tk.Button(topWindow, width=5, text='X-', bg='red')
+BtnYMinus = tk.Button(topWindow, width=5, text='Y-', bg='red')
+BtnYPlus = tk.Button(topWindow, width=5, text='Y+', bg='red')
+BtnHome = tk.Button(topWindow, width=5, height=2, text='HOME', bg='red')
 
 # =============================================================================
 # Buttons layout
 # =============================================================================
-Btn07.grid(row=1,column=5)
-Btn08.grid(row=1,column=3)
-Btn09.grid(row=2,column=4)
-Btn10.grid(row=0,column=4)
-Btn11.grid(row=1,column=4)
+BtnXPlus.grid(row=1, column=5)
+BtnXMinus.grid(row=1, column=3)
+BtnYMinus.grid(row=2, column=4)
+BtnYPlus.grid(row=0, column=4)
+BtnHome.grid(row=1, column=4)
 
 # =============================================================================
 # Bind button events
 # =============================================================================
-Btn07.bind('<ButtonPress-1>', x_increase)
-Btn08.bind('<ButtonPress-1>', x_decrease)
-Btn09.bind('<ButtonPress-1>', y_decrease)
-Btn10.bind('<ButtonPress-1>', y_increase)
-Btn11.bind('<ButtonPress-1>', xy_home)
+BtnXPlus.bind('<ButtonPress-1>', x_increase)
+BtnXMinus.bind('<ButtonPress-1>', x_decrease)
+BtnYMinus.bind('<ButtonPress-1>', y_decrease)
+BtnYPlus.bind('<ButtonPress-1>', y_increase)
+BtnHome.bind('<ButtonPress-1>', xy_home)
 #Btn07.bind('<ButtonRelease-1>', home_fun)
 #Btn08.bind('<ButtonRelease-1>', home_fun)
 #Btn09.bind('<ButtonRelease-1>', home_fun)
@@ -143,15 +147,15 @@ Btn11.bind('<ButtonPress-1>', xy_home)
 # Bind buttons on the keyboard with the corresponding callback function to 
 # control the car remotely with the keyboard.
 # =============================================================================
-top.bind('<KeyPress-a>', left_fun)   # Press down key 'A' on the keyboard and the car will turn left.
-top.bind('<KeyPress-d>', right_fun) 
-top.bind('<KeyPress-s>', backward_fun)
-top.bind('<KeyPress-w>', forward_fun)
-top.bind('<KeyPress-h>', home_fun)
-top.bind('<KeyRelease-a>', home_fun) # Release key 'A' and the car will turn back.
-top.bind('<KeyRelease-d>', home_fun)
-top.bind('<KeyRelease-s>', stop_fun)
-top.bind('<KeyRelease-w>', stop_fun)
+topWindow.bind('<KeyPress-a>', left_fun)   # Press down key 'A' on the keyboard and the car will turn left.
+topWindow.bind('<KeyPress-d>', right_fun)
+topWindow.bind('<KeyPress-s>', backward_fun)
+topWindow.bind('<KeyPress-w>', forward_fun)
+topWindow.bind('<KeyPress-h>', home_fun)
+topWindow.bind('<KeyRelease-a>', home_fun) # Release key 'A' and the car will turn back.
+topWindow.bind('<KeyRelease-d>', home_fun)
+topWindow.bind('<KeyRelease-s>', stop_fun)
+topWindow.bind('<KeyRelease-w>', stop_fun)
 
 spd = 50
 
@@ -164,15 +168,15 @@ def changeSpeed(ev=None):
 	tcpCliSock.send(data.encode())  # Send the speed data to the server(Raspberry Pi)
 
 
-label = tk.Label(top, text='Speed:', fg='red')  # Create a label
+label = tk.Label(topWindow, text='Speed:', fg='red')  # Create a label
 label.grid(row=6, column=0)                  # Label layout
 
-speed = tk.Scale(top, from_=0, to=100, orient=tk.HORIZONTAL, command=changeSpeed)  # Create a scale
+speed = tk.Scale(topWindow, from_=0, to=100, orient=tk.HORIZONTAL, command=changeSpeed)  # Create a scale
 speed.set(50)
 speed.grid(row=6, column=1)
 
 def main():
-	top.mainloop()
+	topWindow.mainloop()
 
 if __name__ == '__main__':
 	main()
--- server/tcp_server.py
+++ server/tcp_server.py
@@ -6,9 +6,10 @@ import motor
 from socket import *
 from time import ctime          # Import necessary modules   
 
-ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'home', 'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
+ctrl_cmd = ['forward', 'backward', 'left', 'right', 'stop', 'read cpu_temp', 'center',
+			'distance', 'x+', 'x-', 'y+', 'y-', 'xy_home']
 
-busnum = 1          # Edit busnum to 0, if you uses Raspberry Pi 1 or 0
+busnum = 1          # Set busnum to 0, if you uses Raspberry Pi 1 or 0
 
 HOST = ''           # The variable of HOST is null, so the function bind( ) can be bound to all valid addresses.
 PORT = 21567
@@ -27,12 +28,12 @@ video_dir.home_x_y()
 car_dir.home()
 
 while True:
-	print 'Waiting for connection...'
+	print('Waiting for connection...')
 	# Waiting for connection. Once receiving a connection, the function accept() returns a separate 
 	# client socket for the subsequent communication. By default, the function accept() is a blocking 
 	# one, which means it is suspended before the connection comes.
 	tcpCliSock, addr = tcpSerSock.accept() 
-	print '...connected from :', addr     # Print the IP address of the client connected with the server.
+	print('...connected from :', addr )    # Print the IP address of the client connected with the server.
 
 	while True:
 		data = ''
@@ -41,69 +42,69 @@ while True:
 		if not data:
 			break
 		if data == ctrl_cmd[0]:
-			print 'motor moving forward'
+			print('motor moving forward')
 			motor.forward()
 		elif data == ctrl_cmd[1]:
-			print 'recv backward cmd'
+			print('recv backward cmd')
 			motor.backward()
 		elif data == ctrl_cmd[2]:
-			print 'recv left cmd'
+			print('recv left cmd')
 			car_dir.turn_left()
 		elif data == ctrl_cmd[3]:
-			print 'recv right cmd'
+			print('recv right cmd')
 			car_dir.turn_right()
 		elif data == ctrl_cmd[6]:
-			print 'recv home cmd'
+			print('recv home cmd')
 			car_dir.home()
 		elif data == ctrl_cmd[4]:
-			print 'recv stop cmd'
+			print('recv stop cmd')
 			motor.ctrl(0)
 		elif data == ctrl_cmd[5]:
-			print 'read cpu temp...'
+			print('read cpu temp...')
 			temp = cpu_temp.read()
 			tcpCliSock.send('[%s] %0.2f' % (ctime(), temp))
 		elif data == ctrl_cmd[8]:
-			print 'recv x+ cmd'
+			print('recv x+ cmd')
 			video_dir.move_increase_x()
 		elif data == ctrl_cmd[9]:
-			print 'recv x- cmd'
+			print('recv x- cmd')
 			video_dir.move_decrease_x()
 		elif data == ctrl_cmd[10]:
-			print 'recv y+ cmd'
+			print('recv y+ cmd')
 			video_dir.move_increase_y()
 		elif data == ctrl_cmd[11]:
-			print 'recv y- cmd'
+			print('recv y- cmd')
 			video_dir.move_decrease_y()
 		elif data == ctrl_cmd[12]:
-			print 'home_x_y'
+			print('home_x_y')
 			video_dir.home_x_y()
 		elif data[0:5] == 'speed':     # Change the speed
-			print data
+			print(data)
 			numLen = len(data) - len('speed')
 			if numLen == 1 or numLen == 2 or numLen == 3:
 				tmp = data[-numLen:]
-				print 'tmp(str) = %s' % tmp
+				print('tmp(str) = %s' % tmp)
 				spd = int(tmp)
-				print 'spd(int) = %d' % spd
+				print('spd(int) = %d' % spd)
 				if spd < 24:
 					spd = 24
 				motor.setSpeed(spd)
 		elif data[0:5] == 'turn=':	#Turning Angle
-			print 'data =', data
+			print('data =', data)
 			angle = data.split('=')[1]
 			try:
 				angle = int(angle)
 				car_dir.turn(angle)
 			except:
-				print 'Error: angle =', angle
+				print('Error: angle =', angle)
 		elif data[0:8] == 'forward=':
-			print 'data =', data
+			print('data =', data)
 			spd = data[8:]
 			try:
 				spd = int(spd)
 				motor.forward(spd)
 			except:
-				print 'Error speed =', spd
+				print('Error speed =', spd)
                 elif data[0:9] == 'backward=':
                         print 'data =', data
                         spd = data.split('=')[1]
@@ -111,10 +112,10 @@ while True:
 				spd = int(spd)
 	                        motor.backward(spd)
 			except:
-				print 'ERROR, speed =', spd
+				print('ERROR, speed =', spd)
 
 		else:
-			print 'Command Error! Cannot recognize command: ' + data
+			print('Command Error! Cannot recognize command: ' + data)
 
 tcpSerSock.close()
 
